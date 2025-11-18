16d78c53435a6d38459cc8b3bec4d21aeff65f4d
PatJRobinson <patrick.robinson@calyo.co.uk>
2025-11-18 17:53:03 +0000

    WIP fix problems

--- distros/humble/rmw-cyclonedds-cpp/default.nix
+++ distros/humble/rmw-cyclonedds-cpp/default.nix
@@ -19,6 +19,14 @@ buildRosPackage {
   propagatedBuildInputs = [ cyclonedds iceoryx-binding-c rcpputils rcutils rmw rmw-dds-common rosidl-runtime-c rosidl-typesupport-introspection-c rosidl-typesupport-introspection-cpp tracetools ];
   nativeBuildInputs = [ ament-cmake-ros ];
 
+  postFixup = ''
+    # Remove double slashes in pkg-config files
+    for pc in $out/lib/pkgconfig/*.pc; do
+      substituteInPlace "$pc" \
+        --replace "//nix/store" "/nix/store"
+    done
+  '';
+
   meta = {
     description = ''Implement the ROS middleware interface using Eclipse CycloneDDS in C++.'';
     license = with lib.licenses; [ asl20 ];
